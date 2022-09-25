23631aed5754abac7a72e1aa4787bacb9c3ce718
Daniel Pathammavong <patha037@umn.edu>
2022-09-25 09:45:26 -0500

    Created logic for form info

--- client.js
+++ client.js
@@ -1,2 +1,25 @@
 console.log('in client.js');
 
+$(document).ready(onReady);
+
+let eArray =[];
+
+function onReady() {
+    console.log("DOM is loaded!");
+    $('.eForm').on('submit', submitInfo);
+}
+
+function submitInfo(event){
+    event.preventDefault();
+
+    let newObject = {
+        firstName: $('#fnInput').val(),
+        lastName: $('#lnInput').val(),
+        ID: $('#IDInput').val(),
+        Title: $('#titleInput').val(),
+        annualSalary: $('#asInput').val()
+    };
+    eArray.push(newObject);
+    console.log('eArray is now ',eArray);
+
+}
